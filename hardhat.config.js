293789aa83de31b8daac8c0bc4f414626e44c4d7
srehan1 <syed.rehan@shoppertrak.com>
2023-06-08 15:51:01 +0530

    Installing dependencies

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-verify");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.16",
+};
