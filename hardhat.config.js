3b0632af9ece50fe200366396204215d7cb1e457
srehan1 <syed.rehan@shoppertrak.com>
2023-06-15 18:49:24 +0530

    Change in compiler version

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,7 +16,7 @@ const COIN_MARKET_KEY = process.env.COIN_MARKET_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.16",
+  solidity: "0.8.18",
   networks: {
     sepolia: {
       url: SEPOLIA_RPC_URL,
